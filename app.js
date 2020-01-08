d6106896ff24bcb9a1ca4cc9a76a714adbb2aff3
Janita M <48458664+JanitaM@users.noreply.github.com>
2020-01-07 20:08:27 -0500

    instead of shuffling the array, rewrote the code to pick a random index and display that word. Cleaned up IDK function and setTimeout

--- app.js
+++ app.js
@@ -18,58 +18,95 @@ let
   feedbackDisplay = document.getElementById("feedback");
 
 // Functions
-// Jumble function - shuffles the array and word
+// Jumble function - removes a random word from the array and shuffles the letters in the word
 const jumble = () => {
-  //shuffle the current words array
+  let index = Math.floor(Math.random() * words.length);  //pick a random index
+  state.usedWords = words.splice(index, 1); //remove word at random index - splice returns an array
+  state.currentWord = state.usedWords.shift();  //remove the first word in the array
 
-  // //take the first word off the array, convert it to lower case
-  state.currentWord = words.shift().toLowerCase();
+  //take currentword convert it to lower case
 
   // shuffle the letters around and save to state.jumbleWord
+  //saving the word to be shuffled
 
-  jumbleDisplay.innerHTML = state.currentWord; //this will display shuffled version
+
+  // index = Math.floor(Math.random() * word.length);
+
+  jumbleDisplay.innerHTML = state.currentWord; //change this to display shuffled version which is in state.jumbledWord
+
+  if (words.length === 0) {  //if there are no words left
+    jumbleDisplay.innerHTML = "";  //clear the display
+    feedbackDisplay.innerHTML = `Game over!`;  //show this msg
+  }
 }
 
 // Start the game
 const start = () => {
-  startBtn.style.display = "none";  // Hide the start button
-  // Show the submit and idk button
+  startBtn.style.display = "none";  //hide the start button
+  // show the submit and idk button
   submitBtn.style.display = "inline-block";
   idkBtn.style.display = "inline-block";
-  jumble(); // Display the first word by calling the jumble function
+  jumble(); //shuffled the word and display it
 }
 
 // Submit function
 const submit = () => {
   userDisplay.innerHTML = userDisplay.value;  //get user's answer
   state.userGuess = userDisplay.value.toLowerCase();  //convert to lowercase and save
-
   if (state.userGuess === state.currentWord) {  //if they match
     feedbackDisplay.innerHTML = `That's it! Try this one`;  //show this msg
+
+    //set a timer on how long the feedback displays
+    setTimeout(function () {
+      feedbackDisplay.innerHTML = ""; //remove the feedback msg
+      jumble();  //show new word
+    }, 1500);
+
     userDisplay.value = "";  //clear the display
     status.userGuess = "";  //clear the guess
   } else if (state.userGuess !== state.currentWord) {  //if they do not match
     feedbackDisplay.innerHTML = `Nope. Try again.`;  //show this msg
     userDisplay.value = "";  //clear the display
+    //leave feedbak up
   }
-
-  // https://stackoverflow.com/questions/22655144/how-to-display-a-message-for-a-few-seconds-and-then-disappear-in-javascript
-  setTimeout(function () {
-    feedbackDisplay.innerHTML = ""; //remove the feedback msg
-    //if this is true, show new word
-    if (state.userGuess === state.currentWord) {
-      jumble();
-    }
-  }, 1500);
 }
 
 // Clicking the "OK, IDK" button returns the answer which is in state.currentWord. It returns the answer to the hidden feedback div.
 const idk = () => {
-  feedbackDisplay.innerHTML = `It was: ${state.currentWord}`;
-  setTimeout()
+  feedbackDisplay.innerHTML = `It was: ${state.currentWord}.`
+  setTimeout(function () {
+    feedbackDisplay.innerHTML = ""; //remove the feedback msg
+    jumble();  //show new word
+  }, 1500);
 }
 
 // Event Listeners
 startBtn.addEventListener("click", start);
 submitBtn.addEventListener("click", submit);
 idkBtn.addEventListener("click", idk);
+
+
+
+/*
+function shuffelWord (word){
+    var shuffledWord = '';
+    word = word.split('');
+    while (word.length > 0) {
+      shuffledWord +=  word.splice(word.length * Math.random() << 0, 1);
+    }
+    return shuffledWord;
+}
+*/
+
+/*
+const shuffelWord = () => {
+    var shuffledWord = '';
+    word = word.split('');
+    while (word.length > 0) {
+      shuffledWord +=  word.splice(word.length * Math.random() << 0, 1);
+    }
+    return shuffledWord;
+}
+
+
+*/
